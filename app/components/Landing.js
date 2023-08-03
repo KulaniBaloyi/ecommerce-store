12863f8aca3a72caf3c563da248314b7c28e03f1
KulaniBaloyi <kulani17.kb@gmail.com>
2023-08-03 22:52:29 +0200

    switched from google cloud to cloudinary

--- app/components/Landing.js
+++ app/components/Landing.js
@@ -8,7 +8,7 @@ const Landing = () => {
     <div className="rounded-xl relative aspect-square md:aspect-video w-full overflow-hidden grid place-content-center">
       <section className="absolute top-1/2 left-1/2 -translate-x-1/2 gap-5 -translate-y-1/2 w-1/2 z-[99] flex flex-col flex-wrap">
         <p className="text-white text-3xl font-semibold ">
-        The best free stock videos shared by the Pexels community.
+        The best free stocks videos shared by the Pexels community.
         </p>
         <div className="bg-white rounded-lg w-full h-12"></div>
       </section>
